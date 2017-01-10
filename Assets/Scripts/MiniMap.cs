41f08c98a25ff811fb418d69c9e60de72b8ffe08
ChoiIngon <kukuta@gmail.com>
2017-01-10 15:46:20 +0900

    해상도 변경시 미니맵도 같이 변경 되도록 변경

--- Assets/Scripts/MiniMap.cs
+++ Assets/Scripts/MiniMap.cs
@@ -23,8 +23,8 @@ public class MiniMap : MonoBehaviour {
 		}  
 	}
 
-	public const float ROOM_SIZE = 19.0f;
-
+	public float ROOM_SIZE = 19.0f;
+    public float size = 0.0f;
 	public GameObject roomPrefab;
 	public Image currentPosition;
 	private GameObject[] rooms;
@@ -33,12 +33,17 @@ public class MiniMap : MonoBehaviour {
 	{
 		int roomID = 0;
 		RectTransform t = GetComponent<RectTransform> ();
-		t.sizeDelta = new Vector2(ROOM_SIZE * Map.WIDTH, ROOM_SIZE * Map.HEIGHT);
+        t.sizeDelta = new Vector2(t.rect.height, t.sizeDelta.y);
+        ROOM_SIZE = t.rect.height / Map.HEIGHT;
 		rooms = new GameObject[Map.WIDTH * Map.HEIGHT];
-		for (int y = 0; y < Map.HEIGHT; y++) {
+        float scale = ROOM_SIZE / 19.0f;
+        currentPosition.GetComponent<RectTransform>().localScale = new Vector3(scale, scale, 1.0f);
+
+        for (int y = 0; y < Map.HEIGHT; y++) {
 			for (int x = 0; x < Map.WIDTH; x++) {
 				GameObject obj = GameObject.Instantiate<GameObject> (roomPrefab);
 				obj.transform.SetParent (transform);
+                obj.transform.localScale = new Vector3(scale, scale, 1.0f);
 				obj.transform.localPosition = new Vector3 (x * ROOM_SIZE - ROOM_SIZE*Map.WIDTH, -y * ROOM_SIZE, 0.0f);
 				obj.transform.FindChild ("Room/NorthDoor").gameObject.SetActive (true);
 				obj.transform.FindChild ("Room/EastDoor").gameObject.SetActive (true);
