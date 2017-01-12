2a1a10218046d8a80d355517c34e34fb073e54cd
ChoiIngon <kukuta@gmail.com>
2017-01-12 17:08:49 +0900

    type writer 스크립트 추가

---
+++ Assets/Scripts/Util/UITextTypeWrite.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(Text))]
+public class UITextTypeWrite : MonoBehaviour {
+    Text text;
+    string copied;
+    public float charPerSecond = 20.0f;
+    void Awake()
+    {
+        text = GetComponent<Text>();
+        copied = text.text;
+        text.text = "";
+
+        StartCoroutine(Play());
+    }
+
+
+    public IEnumerator Play()
+    {
+        foreach (char c in copied)
+        {
+            text.text += c;
+            yield return new WaitForSeconds(1.0f / charPerSecond);
+        }
+    }
+}
