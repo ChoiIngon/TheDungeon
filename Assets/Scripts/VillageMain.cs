64881498373c51c00508a292c252d8dc08135c3a
kukuta <kukuta@gmail.com>
2017-01-22 16:53:14 +0900

    아몰랑..일단 일단락..

---
+++ Assets/Scripts/VillageMain.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class VillageMain : MonoBehaviour {
+	Button start;
+	// Use this for initialization
+	void Start () {
+		start = GetComponent<Button> ();
+		start.onClick.AddListener(() => {
+			SceneManager.LoadScene("Dungeon");
+		});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
