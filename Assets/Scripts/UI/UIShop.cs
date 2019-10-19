81f179de1cee9bddb50b1d7d04361a378ce9d668
ChoiIngon <kukuta@gmail.com>
2019-10-20 02:44:23 +0900

    상점 추가 중

---
+++ Assets/Scripts/UI/UIShop.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIShop : MonoBehaviour
+{
+	private Button close;
+
+	private void Awake()
+	{
+		close = UIUtil.FindChild<Button>(transform, "BottomBar/Close");
+		UIUtil.AddPointerUpListener(close.gameObject, () => { gameObject.SetActive(false); });
+	}
+
+	public void Init()
+	{
+		for (int i = 0; i < transform.childCount; i++)
+		{
+			Transform child = transform.GetChild(i);
+			Item.Meta meta = ItemManager.Instance.GetRandomExpendableItemMeta();
+			Sprite sprite = ResourceManager.Instance.Load<Sprite>(meta.sprite_path);
+		}
+	}
+}
